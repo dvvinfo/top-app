3400b9beba15465891da1693637ed7bea4d2bb64
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-07-18 13:27:28 +0300

    добавил верстку компонента Footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import styles from "./Footer.module.css";
 import { FooterProps } from "./Footer.props";
-
-export const Footer = ({ ...props }: FooterProps) => {
+import cn from "classnames";
+import {format} from 'date-fns'
+export const Footer = ({ className, ...props }: FooterProps) => {
   return (
-    <>
-      <div {...props}>Footer</div>
-    </>
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop 2022 - {format(new Date(), "yyy")} все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
   );
 };
