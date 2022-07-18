50008c02a5498be45667a42458c0e32bf76b3e1e
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-07-18 11:51:01 +0300

    добавил логику комнонента Layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,18 +3,29 @@ import styles from "./Layout.module.css";
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
+import { FunctionComponent } from "react";
 
-export const Layout= ({children}: LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <div>
+        <Sidebar />
+        <div>{children}</div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export const withLayout = <T extends Record<string, unknown>>(
+  Component: FunctionComponent<T>
+) => {
+  return function withLayoutComponent(props: T): JSX.Element {
     return (
-        <>
-        <Header/>
-        <div>
-            <Sidebar/>
-            <div>
-                {children}
-            </div>
-        </div>
-        <Footer/>
-        </>
-    )
-}
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,39 @@ import Htag from "../components/HTag/Htag";
 import P from "../components/P/P";
 import Rating from "../components/Rating/Rating";
 import Tag from "../components/Tag/Tag";
-import { Layout } from "../layout/Layout";
+import { withLayout } from "../layout/Layout";
 
-
-export default function Home():JSX.Element  {
-  const [counter,setCounter] =  useState<number>(0); 
+function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
   const [rating, setRating] = useState<number>(4);
 
   return (
-    <Layout>
-      <Htag tag ='h1'>{counter}</Htag>
-      <Button appearance='primary'
-       onClick={() => setCounter(x => x + 1)}>кнопка</Button>
-      <Button appearance='ghost' arrow="right">кнопка</Button>
+    <>
+      <Htag tag="h1">{counter}</Htag>
+      <Button appearance="primary" onClick={() => setCounter((x) => x + 1)}>
+        кнопка
+      </Button>
+      <Button appearance="ghost" arrow="right">
+        кнопка
+      </Button>
       <P size="l">Большой</P>
       <P size="m">средний</P>
       <P size="s">Маленький</P>
-      <Tag size="s" color="ghost">ghost</Tag>
-      <Tag size="m" color="red">red</Tag>
-      <Tag size="s" color="green">green</Tag>
-      <Tag size="m" color="primary">primary</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}/>
-
-    </Layout>
-  )
+      <Tag size="s" color="ghost">
+        ghost
+      </Tag>
+      <Tag size="m" color="red">
+        red
+      </Tag>
+      <Tag size="s" color="green">
+        green
+      </Tag>
+      <Tag size="m" color="primary">
+        primary
+      </Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
+    </>
+  );
 }
+
+export default withLayout(Home);
