aafbe0b79e78bf95d04d0dc7e9b992f4f645c845
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-07-19 18:04:26 +0300

    добавил alias

---
+++ interfaces/page.intrefces.ts
@@ -0,0 +1,38 @@
+export enum TopLeverCategory {
+  Courses,
+  Services,
+  Books,
+  products
+}
+
+export interface TopPageAdvantage {
+  _id: string;
+  title: string;
+  description: string;
+}
+
+export interface HhData {
+  _id: string;
+  count: number;
+  juniorSalary: number;
+  middleSalary: number;
+  seniorSalary: number;
+  updatedAt: Date;
+}
+export interface TopPageModel {
+  tabs: string[];
+  _id: string;
+  secondCategory: string;
+  alias: string;
+  title: string;
+  category: string;
+  seoText: string;
+  tagsTitle: string;
+  metaTitle: string;
+  metaDescription: string;
+  firstCategory: TopLeverCategory;
+  advantages: TopPageAdvantage[];
+  createdAt: Date;
+  updatedAt: Date;
+  hh: HhData;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { GetStaticProps } from "next";
+import { useState } from "react";
+import axios from "axios";
 import Button from "../components/Button/Button";
 import Htag from "../components/HTag/Htag";
 import P from "../components/P/P";
 import Rating from "../components/Rating/Rating";
 import Tag from "../components/Tag/Tag";
 import { withLayout } from "../layout/Layout";
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [counter, setCounter] = useState<number>(0);
   const [rating, setRating] = useState<number>(4);
 
@@ -35,8 +38,31 @@ function Home(): JSX.Element {
         primary
       </Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find", {firstCategory}
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
